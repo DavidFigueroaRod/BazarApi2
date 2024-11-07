c17513ee4cd48fb5caae5c8f6bee639d186d77b6
David Figueroa Rodríguez <david.figueroar0777@gmail.com>
2024-11-06 21:21:35 -0600

    modificacion api ventas

--- main.py
+++ main.py
@@ -121,7 +121,8 @@ def get_sales():
     conn.close()
     if not sales:
         raise HTTPException(status_code=404, detail="No sales found")
-    return sales
+
+    return {"sales": sales}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
